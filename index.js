09d3035928630e17e2ce67d1e48fcbc69f469cbb
nitesh <niteshkymar1@gmail.com>
2023-08-15 11:08:12 +0530

    after using request-ip

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express')
-
 const app = express()
+const requestIp = require('request-ip');
 
+app.use(requestIp.mw());
 app.get("/",(req,res)=>{
-    res.status(201).send(`your ip address is ${req.ip}`)
+    res.status(201).send(`it is your ip address is ${req.clientIp}`)
 })
 
-app.listen(3200)
+
+app.listen(3200,()=>{
+    console.log("the server is running");
+})
